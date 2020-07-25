e2ea91b517e6a94d7e59d51425a8ae5592aa471c
Afzal Ahmed <52744257+afzalahmed81@users.noreply.github.com>
2020-07-25 21:39:10 +0500

    web-animation repo created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { TweenMax } from "gsap";
+import heli from './heli.gif';
 
-function App() {
+import "./App.css";
+
+
+
+const App = () => {
+  let imgRef = useRef(null);
+
+  const [animation, setAnimation] = useState(null);
+
+  useEffect(() => {
+    setAnimation(
+      TweenMax.to(imgRef, 10, {
+        rotation: 360,
+        repeat: -1
+      }).pause()
+    );
+  }, []);
+
+
+
   return (
-    <div className="App">
+    
+    
+      <div className="App">  
+      
+      
+                                 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <h5>Simple web animation with react hooks, rotate the helicopter</h5>
+        <img
+          src={heli}
+          alt={"heli-animation"}
+          className="Rotating-Heli"
+          
+          ref={element => {
+            imgRef = element;
+          }}
+        />
+        
+        <div>
+          <button onClick={() => animation.play()}>Play</button>
+          <button onClick={() => animation.pause()}>Pause</button>
+          <button onClick={() => animation.reverse()}>Reverse</button>
+        </div>
+
+                  
+      
+          
+       </header>
+        
+       </div>
+      
+    
+    
   );
-}
+};
 
 export default App;
